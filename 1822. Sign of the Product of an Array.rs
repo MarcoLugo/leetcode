84c333d21feb524317846377ee71dee81ef82bef
Marco Lugo <>
2021-04-16 21:11:55 -0400

    add problem 1822

---
+++ 1822. Sign of the Product of an Array.rs
@@ -0,0 +1,23 @@
+/*
+There is a function signFunc(x) that returns:
+
+    1 if x is positive.
+    -1 if x is negative.
+    0 if x is equal to 0.
+
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+Constraints:
+
+    1 <= nums.length <= 1000
+    -100 <= nums[i] <= 100
+*/
+
+impl Solution {
+    pub fn array_sign(nums: Vec<i32>) -> i32 {
+        nums.into_iter().map(|x| x.signum()).product()
+    }
+}
+
